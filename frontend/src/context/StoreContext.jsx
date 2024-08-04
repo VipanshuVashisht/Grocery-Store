b9f6f1865364354d289425bd564d0ff30b411804
Vipanshu Vashisht <vipanshuvashisht@gmail.com>
2024-08-04 20:46:22 +0530

    backend changed

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,8 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) =>{
 
     const [cartItems, setCartItems] = useState({})
-    const url = 'http://localhost:4000'
+    // const url = 'http://localhost:4000'
+    const url = 'https://grocery-store-v1mt.onrender.com/'
     const [token, setToken] = useState("")
     const [food_list, setFoodList] = useState([])
 
